0e71a28ca29442f4430e024649783cff90d2f032
Eliot McIntire <eliotmci@mail.ubc.ca>
2023-09-27 10:43:23 -0700

    bump minimum version of SpaDES.core

--- R/spades_modules/cbm_exn_spinup/cbm_exn_spinup.r
+++ R/spades_modules/cbm_exn_spinup/cbm_exn_spinup.r
@@ -31,7 +31,7 @@ defineModule(
     documentation = list("README.md"),
     reqdPkgs = list(
       "PredictiveEcology/reproducible@development (>= 2.0.8.9001)",
-      "PredictiveEcology/SpaDES.core@defineModule (>= 2.0.2.9005)",
+      "PredictiveEcology/SpaDES.core@development (>= 2.0.2.9005)",
       "PredictiveEcology/LandR@development"
     ),
     parameters = rbind(
--- tests/testthat/spades_modules/test_spinup_spades_integration.r
+++ tests/testthat/spades_modules/test_spinup_spades_integration.r
@@ -11,41 +11,41 @@ test_that(
     get_test_net_increments <- function() {
       net_increments <- read.csv(
         file.path(
-      libcbm_resources$get_test_resources_dir(),
-      "cbm_exn_net_increments",
-      "net_increments.csv"
-    )
-  )
-
-  colnames(net_increments) <- c(
-    "age", "merch_inc", "foliage_inc", "other_inc"
-  )
-  stand_increments <- NULL
-  n_stands <- 2
-  for (i in 0:(n_stands - 1)) {
-    copied_increments <- data.frame(net_increments)
-    copied_increments <- cbind(data.frame(row_idx = i), copied_increments)
-    stand_increments <- rbind(
-      stand_increments, copied_increments
-    )
-  }
-  return(stand_increments)
-}
-
-get_test_spinup_parameters <- function(){
-  n_stands <- 2
-  spinup_parameters <- data.frame(
-    age = sample(0L:60L, n_stands, replace = TRUE),
-    area = rep(1.0, n_stands),
-    delay = rep(0L, n_stands),
-    return_interval = rep(125L, n_stands),
-    min_rotations = rep(10L, n_stands),
-    max_rotations = rep(30L, n_stands),
-    spatial_unit_id = rep(17L, n_stands), # Ontario/Mixedwood plains
-    species = rep(20L, n_stands), # red pine
-    mean_annual_temperature = rep(2.55, n_stands),
-    historical_disturbance_type = rep(1L, n_stands),
-    last_pass_disturbance_type = rep(1L, n_stands)
+          libcbm_resources$get_test_resources_dir(),
+          "cbm_exn_net_increments",
+          "net_increments.csv"
+        )
+      )
+      
+      colnames(net_increments) <- c(
+        "age", "merch_inc", "foliage_inc", "other_inc"
+      )
+      stand_increments <- NULL
+      n_stands <- 2
+      for (i in 0:(n_stands - 1)) {
+        copied_increments <- data.frame(net_increments)
+        copied_increments <- cbind(data.frame(row_idx = i), copied_increments)
+        stand_increments <- rbind(
+          stand_increments, copied_increments
+        )
+      }
+      return(stand_increments)
+    }
+    
+    get_test_spinup_parameters <- function(){
+      n_stands <- 2
+      spinup_parameters <- data.frame(
+        age = sample(0L:60L, n_stands, replace = TRUE),
+        area = rep(1.0, n_stands),
+        delay = rep(0L, n_stands),
+        return_interval = rep(125L, n_stands),
+        min_rotations = rep(10L, n_stands),
+        max_rotations = rep(30L, n_stands),
+        spatial_unit_id = rep(17L, n_stands), # Ontario/Mixedwood plains
+        species = rep(20L, n_stands), # red pine
+        mean_annual_temperature = rep(2.55, n_stands),
+        historical_disturbance_type = rep(1L, n_stands),
+        last_pass_disturbance_type = rep(1L, n_stands)
       )
       return(spinup_parameters)
     }
@@ -71,6 +71,7 @@ get_test_spinup_parameters <- function(){
       ),
       modules = "cbm_exn_spinup",
       times = list(start = 1998, end = 2000),
+      packages = "PredictiveEcology/SpaDES.core@development (>= 2.0.2.9005)",
 
       spinup_parameters = get_test_spinup_parameters(),
       stand_increments = get_test_net_increments(),
