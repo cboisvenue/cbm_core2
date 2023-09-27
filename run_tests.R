508f08cc8f5693b57dc30e6745d3cfdb312ef74d
Scott Morken <scottm361@gmail.com>
2023-09-27 16:17:37 -0700

    use the test_ pattern again

--- run_tests.R
+++ run_tests.R
@@ -1,6 +1,6 @@
 result <- lapply(
   list.files(
-    path = "tests", pattern = "spades_", recursive = TRUE, full.names = TRUE
+    path = "tests", pattern = "test_", recursive = TRUE, full.names = TRUE
   ),
   testthat::test_file
 )
