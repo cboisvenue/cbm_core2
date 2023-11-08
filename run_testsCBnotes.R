291cb6e184ae74d67a2136c17cc6d2d0742454ae
Celine Boisvenue <cboisvenue@gmail.com>
2023-11-08 15:55:04 -0800

    Updated with needed steps to run tests from PFC

--- run_testsCBnotes.R
+++ run_testsCBnotes.R
@@ -1,33 +1,38 @@
 ## CB trying to figure out how to get the latest version of libcbm (2.6.0)
 install.packages("reticulate")
 
-###NOTE This is to install python - correct?
-## from libcbmr README.md lines 68-76 ###########################
+
 library(reticulate)
-###NOTES this makes sure the path is set
 
-condaVersion <- tryCatch(conda_version(conda = "auto"),
-                         error = function(e) NA_character_)
-if (is.na(condaVersion)) {
-  reticulate::install_miniconda() ## full path cannot contain spaces!
-  conda_create("r-reticulate")
-}
+###NOTE: For Python installation see libcbmr repo (https://github.com/smorken/libcbmr)
 
+###NOTES This will tell you where the Rstudio session is finding Python.
+### If using approach in libcbmr, it might be a different path "r-reticulate"
 
+#reticulate::import("sys")$executable
 
-###NOTES From Scott on Zulip
-## This will tell you where the Rstudio session is finding Python.
-## If using AChubaty's approach in libcbmr, it might be a different path "r-reticulate"
-reticulate::import("sys")$executable
+### Check what is the virtual environment being used
+virtualenv_list()
+#[1] "r-reticulate"
+virtualenv_python()
+#[1] "C:/Users/cboisven/Documents/.virtualenvs/r-reticulate/Scripts/python.exe"
+
+### set your virutal environment to avoid confusion.
+use_virtualenv("r-reticulate")
 
 ## or
 #library(reticulate)
 #reticulate::use_virtualenv("C:\\Users\\smorken\\dev\\python\\py311")
-##NOTE mine would be "C:\\Users\\cboisven\\AppData\\Local\\R-MINI~1\\envs\\R-RETI~1\\python.exe"
+##NOTE mine would be "C:/Users/cboisven/Documents/.virtualenvs/r-reticulate/Scripts/python.exe""
 
 ###NOTES This gets the latest version of libcbm that Scott is maintaining
 libcbm <- reticulate::import("libcbm")
-print(reticulate::py_get_attr(libcbm, "__version__"))
+
+###NOTES If you already had a version loaded you may have to do it this way
+libcbm <- reticulate::py_install("libcbm", envname = "r-reticulate", ignore_installed=TRUE)
+
+###NOTES Check what you have
+#print(reticulate::py_get_attr(libcbm, "__version__"))
 
 ###NOTES This package is used in the testthat scripts Scott has
 install.packages("box")
@@ -44,6 +49,9 @@ getOrUpdatePkg("SpaDES.project", "0.0.8.9026")
 install.packages("devtools")
 devtools::install_github("smorken/libcbmr")
 library(libcbmr)
+###NOTES Not sure why the raster package is still being used and why it is not
+###getting loaded.
+library(raster)
 
 result <- lapply(
   list.files(
