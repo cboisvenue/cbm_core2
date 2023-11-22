57b43d4780ab43de8cae03f77fe00d916378beb4
Scott Morken <scottm361@gmail.com>
2023-11-22 10:07:47 -0800

    update integration test

--- tests/testthat/test_cbm_exn_spinup_integration.r
+++ tests/testthat/test_cbm_exn_spinup_integration.r
@@ -4,10 +4,7 @@ test_that(
     box::use(SpaDES.project)
     box::use(testthat[test_that, expect_equal])
     box::use(reticulate[reticulate_import = import])
-
     libcbm_resources <- reticulate_import("libcbm.resources")
-
-    param_path <- libcbm_resources$get_cbm_exn_parameters_dir()
     n_stands <- 2
     net_increments <- read.csv(
       file.path(
@@ -20,16 +17,17 @@ test_that(
     colnames(net_increments) <- c(
       "age", "merch_inc", "foliage_inc", "other_inc"
     )
-    stand_increments <- NULL
+    growth_increments <- NULL
     n_stands <- 2
     for (i in 0:(n_stands - 1)) {
       copied_increments <- data.frame(net_increments)
       copied_increments <- cbind(data.frame(row_idx = i), copied_increments)
-      stand_increments <- rbind(
-        stand_increments, copied_increments
+      growth_increments <- rbind(
+        growth_increments, copied_increments
       )
     }
 
+    # dummy inventory for testing
     spinup_parameters <- data.frame(
       age = sample(0L:60L, n_stands, replace = TRUE),
       area = rep(1.0, n_stands),
@@ -38,12 +36,22 @@ test_that(
       min_rotations = rep(10L, n_stands),
       max_rotations = rep(30L, n_stands),
       spatial_unit_id = rep(17L, n_stands), # Ontario/Mixedwood plains
+      sw_hw = rep(0L, n_stands),
       species = rep(20L, n_stands), # red pine
       mean_annual_temperature = rep(2.55, n_stands),
       historical_disturbance_type = rep(1L, n_stands),
       last_pass_disturbance_type = rep(1L, n_stands)
     )
 
+    spinup_ops <- libcbmr::cbm_exn_spinup_ops(
+      dict(
+        parameters = spinup_parameters,
+        increments = growth_increments
+      ),
+      libcbmr::cbm_exn_get_default_parameters()
+    )
+
+    spinup_op_sequence <- libcbmr::cbm_exn_get_spinup_op_sequence()
     out <- SpaDES.project::setupProject(
       name = "cbm_exn_spinup_integration_test",
       paths = list(
@@ -70,33 +78,15 @@ test_that(
       packages = "PredictiveEcology/SpaDES.core@development (>= 2.0.2.9005)",
 
       spinup_parameters = spinup_parameters,
-      stand_increments = stand_increments,
+      growth_increments = growth_increments,
+      spinup_ops = spinup_ops,
+      spinup_op_sequence = spinup_op_sequence,
+      parameters = libcbmr::cbm_exn_get_default_parameters(),
       require = "PredictiveEcology/SpaDES.core@development",
-      # add the defualt parameters for integration testing purposes
-      slow_mixing_rate = read.csv(
-        file.path(param_path, "slow_mixing_rate.csv")
-      ),
-      turnover_parameters = read.csv(
-        file.path(param_path, "turnover_parameters.csv")
-      ),
-      species = read.csv(
-        file.path(param_path, "species.csv")
-      ),
-      root_parameters = read.csv(
-        file.path(param_path, "root_parameters.csv")
-      ),
-      decay_parameters = read.csv(
-        file.path(param_path, "decay_parameters.csv")
-      ),
-      disturbance_matrix_value = read.csv(
-        file.path(param_path, "disturbance_matrix_value.csv")
-      ),
-      disturbance_matrix_association = read.csv(
-        file.path(param_path, "disturbance_matrix_association.csv")
-      )
     )
 
     result <- do.call(simInitAndSpades, out)
     testthat::expect_equal(nrow(result$pools), 2)
+    print(result$pools)
   }
 )
